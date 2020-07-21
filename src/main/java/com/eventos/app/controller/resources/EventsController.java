7cd5cff35a3c0cb001132e92d0466996c556fe76
Set <6942774+setzerbomb@users.noreply.github.com>
2020-07-21 01:30:50 -0300

    Global exception handling

--- src/main/java/com/eventos/app/controller/resources/EventsController.java
+++ src/main/java/com/eventos/app/controller/resources/EventsController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
@@ -39,29 +40,37 @@ public class EventsController {
     }
 
     @PostMapping
-    public ResponseEntity<Event> create(@Valid @RequestBody EventDTO eventDTO, BindingResult error) throws DataException,ValidationException {
+    public ResponseEntity<Event> create(@Valid @RequestBody EventDTO eventDTO, BindingResult error) throws ResponseStatusException {
         Authentication authentication = authenticationFacade.getAuthentication();
         if (authentication != null) {
             if (!error.hasErrors()) {
                 eventDTO.setUser(usersService.findByEmail(authentication.getName()).getId());
-                return new ResponseEntity<>(eventsService.insert(eventDTO),  HttpStatus.OK);
+                try {
+                    return new ResponseEntity<>(eventsService.insert(eventDTO),  HttpStatus.OK);
+                } catch (DataException e) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+                }
             }
-            throw new ValidationException(error.getFieldError().getDefaultMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,error.getFieldError().getDefaultMessage());
         }
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Event> update(@PathVariable String id, @Valid @RequestBody EventDTO eventDTO, BindingResult error) throws DataException, ValidationException{
+    public ResponseEntity<Event> update(@PathVariable String id, @Valid @RequestBody EventDTO eventDTO, BindingResult error) throws ResponseStatusException {
         Authentication authentication = authenticationFacade.getAuthentication();
         if (authentication != null) {
             String user = usersService.findByEmail(authentication.getName()).getId();
             if (!error.hasErrors()) {
                 eventDTO.setId(id);
                 eventDTO.setUser(user);
-                return new ResponseEntity<>(eventsService.update(eventDTO),  HttpStatus.OK);
+                try {
+                    return new ResponseEntity<>(eventsService.update(eventDTO), HttpStatus.OK);
+                } catch (DataException e) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+                }
             }
-            throw new ValidationException(error.getFieldError().getDefaultMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,error.getFieldError().getDefaultMessage());
         }
         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
