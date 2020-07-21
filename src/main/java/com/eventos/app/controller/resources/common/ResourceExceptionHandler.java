0a9b2810fb9b85836ae35b1d6913f076f760c601
Set <6942774+setzerbomb@users.noreply.github.com>
2020-07-21 16:21:59 -0300

    Teste exception handler

--- src/main/java/com/eventos/app/controller/resources/common/ResourceExceptionHandler.java
+++ src/main/java/com/eventos/app/controller/resources/common/ResourceExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.eventos.app.controller.resources.common;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestController
+@ControllerAdvice
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
 }
