a4bc37f596c04c84268875a168209c9eeb055e9c
Set <6942774+setzerbomb@users.noreply.github.com>
2020-07-21 19:08:37 -0300

    Revert "Teste exception handler"
    This reverts commit 0a9b2810fb9b85836ae35b1d6913f076f760c601.

--- src/main/java/com/eventos/app/controller/resources/common/ResourceExceptionHandler.java
+++ src/main/java/com/eventos/app/controller/resources/common/ResourceExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.eventos.app.controller.resources.common;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestController
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
 }
