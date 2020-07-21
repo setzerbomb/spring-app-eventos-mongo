94a8fe4d86eacecd9c8f74446c2edcb46a0c5c5f
Set <6942774+setzerbomb@users.noreply.github.com>
2020-07-21 01:20:34 -0300

    Global exception handling

--- src/main/java/com/eventos/app/controller/resources/common/ResourceExceptionHandler.java
+++ src/main/java/com/eventos/app/controller/resources/common/ResourceExceptionHandler.java
@@ -5,15 +5,27 @@ import com.eventos.app.common.exceptions.DataException;
 import com.eventos.app.common.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AuthorizationServiceException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.xml.bind.ValidationException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestControllerAdvice
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,10 +37,30 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 		this.messageByLocaleService = messageByLocaleService;
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex,
+			HttpHeaders headers,
+			HttpStatus status,
+			WebRequest request) {
+		List<String> errors = new ArrayList<String>();
+		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+			errors.add(error.getField() + ": " + error.getDefaultMessage());
+		}
+		for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+			errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+		}
+
+		StandardError apiError =
+				new StandardError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+		return handleExceptionInternal(
+				ex, apiError, headers, apiError.getStatus(), request);
+	}
+
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), null);
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	public ResponseEntity<String> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String message = messageByLocaleService.getMessage(e.getMessage());
+		return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(DataException.class)
@@ -42,20 +74,44 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<StandardError> dataIntegrityViolation(DataIntegrityViolationException e,
-                                                                HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-				System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	@ExceptionHandler({ ConstraintViolationException.class })
+	public ResponseEntity<Object> handleConstraintViolation(
+			ConstraintViolationException ex, WebRequest request) {
+		List<String> errors = new ArrayList<String>();
+		for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+			errors.add(violation.getRootBeanClass().getName() + " " +
+					violation.getPropertyPath() + ": " + violation.getMessage());
+		}
+
+		StandardError apiError =
+				new StandardError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+		return new ResponseEntity<Object>(
+				apiError, new HttpHeaders(), apiError.getStatus());
 	}
 
+	@ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+	public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+			MethodArgumentTypeMismatchException ex, WebRequest request) {
+		String error =
+				ex.getName() + " should be of type " + ex.getRequiredType().getName();
 
-	@ExceptionHandler(AuthorizationServiceException.class)
-	public ResponseEntity<StandardError> authorization(AuthorizationServiceException e, HttpServletRequest request) {
-
-		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+		StandardError apiError =
+				new StandardError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), Arrays.asList(error));
+		return new ResponseEntity<Object>(
+				apiError, new HttpHeaders(), apiError.getStatus());
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleMissingServletRequestParameter(
+			MissingServletRequestParameterException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		String error = ex.getParameterName() + " parameter is missing";
+
+		StandardError apiError =
+				new StandardError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), Arrays.asList(error));
+		return new ResponseEntity<Object>(
+				apiError, new HttpHeaders(), apiError.getStatus());
+	}
+
 
 }
--- src/main/java/com/eventos/app/controller/resources/common/StandardError.java
+++ src/main/java/com/eventos/app/controller/resources/common/StandardError.java
@@ -1,43 +1,23 @@
 package com.eventos.app.controller.resources.common;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
+import java.io.Serializable;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Integer status;
+	private HttpStatus status;
 	private String msg;
-	private Long timeStamp;
-
-	public StandardError(Integer status, String msg, Long timeStamp) {
-		super();
-		this.status = status;
-		this.msg = msg;
-		this.timeStamp = timeStamp;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
-	public Long getTimeStamp() {
-		return timeStamp;
-	}
-
-	public void setTimeStamp(Long timeStamp) {
-		this.timeStamp = timeStamp;
-	}
+	private List<String> errors;
 
 }
